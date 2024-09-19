c397c40af777c91e1e79b6737a904de235144cb0
Samuel Corsi-House <sam@chouse.dev>
2024-09-19 12:30:37 -0400

    feat(grade_calc): format

--- assignments/grade_calc/script.py
+++ assignments/grade_calc/script.py
@@ -1,8 +1,10 @@
 def float_input(prompt: str) -> int:
-  return int(input(prompt))
+    return int(input(prompt))
+
 
 def average(nums: list[int]) -> float:
-  return float(sum(nums)) / float(len(nums))
+    return sum(nums) / len(nums)
+
 
 comp_labs = float_input("Enter the number of labs completed: ")
 comp_quizzes = float_input("Enter the number of quizzes completed: ")
@@ -17,12 +19,14 @@ grade_preps = float_input("Enter the grade for Midterms and Final Preparation: "
 
 weighted_avg = 0
 
-weighted_avg += comp_labs / 6 * 100 * .2
-weighted_avg += comp_quizzes / 6 * 100 * .15
-weighted_avg += average([grade_ass_one, grade_ass_two, grade_ass_three, grade_ass_four]) * .16
-weighted_avg += average([grade_ass_one, grade_ass_two]) * .25
-weighted_avg += grade_final * .18
-weighted_avg += grade_preps * .06
+weighted_avg += comp_labs / 6 * 100 * 0.2
+weighted_avg += comp_quizzes / 6 * 100 * 0.15
+weighted_avg += (
+    average([grade_ass_one, grade_ass_two, grade_ass_three, grade_ass_four]) * 0.16
+)
+weighted_avg += average([grade_ass_one, grade_ass_two]) * 0.25
+weighted_avg += grade_final * 0.18
+weighted_avg += grade_preps * 0.06
 
 # how should it be rounded? down or up? decimal places?
 print(round(weighted_avg, 1))
--- assignments/grade_calc/test.py
+++ assignments/grade_calc/test.py
@@ -5,23 +5,22 @@ import re
 
 args = sys.argv[1:]
 if len(args) == 0:
-  print("Missing script argument")
-  sys.exit(1)
+    print("Missing script argument")
+    sys.exit(1)
 
-with open('tests.json', 'r') as file:
-  cases = json.load(file)
+with open("tests.json", "r") as file:
+    cases = json.load(file)
 
 for idx, case in enumerate(cases):
-  s = ""
-  for i in case['inputs']:
-    s += f"{i}\n"
+    process = subprocess.run(
+        ["python", args[0]],
+        input="\n".join(case["inputs"]).encode(),
+        stdout=subprocess.PIPE,
+    )
+    grade = re.search(r"\d+\.\d+$", process.stdout.decode()).group(0)
 
-  process = subprocess.run(["python", args[0]], input=s.encode(), stdout=subprocess.PIPE)
-  grade = re.search(r"\d+\.\d+$", process.stdout.decode()).group(0)
-
-  if grade == case['output']:
-    print(f"Passed test {idx + 1}/{len(cases)}")
-  else:
-    print(f"Failed test {idx + 1}/{len(cases)}")
-    print(f"Got: {grade}, Expected: {case['output']}")
-
+    if grade == case["output"]:
+        print(f"Passed test {idx + 1}/{len(cases)}")
+    else:
+        print(f"Failed test {idx + 1}/{len(cases)}")
+        print(f"Got: {grade}, Expected: {case['output']}")
