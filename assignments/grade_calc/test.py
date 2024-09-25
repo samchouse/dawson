814e7bc1d7c736865224bc6c6ab6cf1ca2a52737
Samuel Corsi-House <sam@chouse.dev>
2024-09-25 15:31:27 -0400

    fix: auto test and add midterm 1

--- assignments/grade_calc/test.py
+++ assignments/grade_calc/test.py
@@ -12,8 +12,12 @@ with open("tests.json", "r") as file:
     cases = json.load(file)
 
 for idx, case in enumerate(cases):
+    is_python_3 = (
+        subprocess.run(["which", "python3"], stdout=subprocess.DEVNULL).returncode == 0
+    )
+
     process = subprocess.run(
-        ["python", args[0]],
+        ["python3" if is_python_3 else "python", args[0]],
         input="\n".join(case["inputs"]).encode(),
         stdout=subprocess.PIPE,
     )
@@ -26,7 +30,7 @@ for idx, case in enumerate(cases):
     else:
         print(f"!!! Failed test {idx + 1}/{len(cases)}")
         print(f"!!! Got: {grade}, Expected: {case['output']}")
-        if abs(float(grade) - float(case["output"])) < 1:
+        if abs(float(grade) - float(case["output"])) <= 1:
             print(
                 "!!! This may be a difference in rounding styles, please verify for yourself"
             )
