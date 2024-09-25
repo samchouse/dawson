dced1bb5f31f3a057397a28647e779cb284274fe
Samuel Corsi-House <sam@chouse.dev>
2024-09-25 15:36:17 -0400

    fix: style and reqs

--- assignments/grade_calc/script.py
+++ assignments/grade_calc/script.py
@@ -1,5 +1,10 @@
-def float_input(wants_grade: bool, suffix: str) -> float:
-    prompt = f"Enter the {f"grade for {suffix}" if wants_grade else f"number of {suffix} completed"}: "
+def int_input(suffix: str) -> float:
+    prompt = f"Enter the number of {suffix} completed: "
+    return float(input(prompt))
+
+
+def float_input(suffix: str) -> float:
+    prompt = f"Enter the grade for {suffix}: "
     return float(input(prompt))
 
 
@@ -11,14 +16,14 @@ weighted_avg = 0
 
 # Pass or fail
 labs_weight = 20
-completed_labs = float_input(False, "labs")
+completed_labs = int_input("labs")
 if completed_labs > 6:
     weighted_avg += weighted(100, labs_weight)
 else:
     weighted_avg += weighted(completed_labs / 6 * 100, labs_weight)
 
 quizzes_weight = 15
-completed_quizzes = float_input(False, "quizzes")
+completed_quizzes = int_input("quizzes")
 if completed_quizzes > 6:
     weighted_avg += weighted(100, quizzes_weight)
 else:
@@ -26,17 +31,17 @@ else:
 
 # Assignments
 assignment_weight = 4
-weighted_avg += weighted(float_input(True, "Assignment 1"), assignment_weight)
-weighted_avg += weighted(float_input(True, "Assignment 2"), assignment_weight)
-weighted_avg += weighted(float_input(True, "Assignment 3"), assignment_weight)
-weighted_avg += weighted(float_input(True, "Assignment 4"), assignment_weight)
+weighted_avg += weighted(float_input("Assignment 1"), assignment_weight)
+weighted_avg += weighted(float_input("Assignment 2"), assignment_weight)
+weighted_avg += weighted(float_input("Assignment 3"), assignment_weight)
+weighted_avg += weighted(float_input("Assignment 4"), assignment_weight)
 
 # Midterms & finals
 midterm_weight = 12.5
-weighted_avg += weighted(float_input(True, "Midterm 1"), midterm_weight)
-weighted_avg += weighted(float_input(True, "Midterm 2"), midterm_weight)
-weighted_avg += weighted(float_input(True, "Final Exam"), 18)
-weighted_avg += weighted(float_input(True, "Midterms and Final Preparation"), 6)
+weighted_avg += weighted(float_input("Midterm 1"), midterm_weight)
+weighted_avg += weighted(float_input("Midterm 2"), midterm_weight)
+weighted_avg += weighted(float_input("Final Exam"), 18)
+weighted_avg += weighted(float_input("Midterms and Final Preparation"), 6)
 
 print()
 print(f"Your final grade is {round(weighted_avg, 2)}%.")
